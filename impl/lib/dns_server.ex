67e1ed88d25d96cd65eb454b7919eb2cf1c33f45
Ian Atha <ian@atha.io>
2020-03-30 21:14:59 -0700

    soa

--- impl/lib/dns_server.ex
+++ impl/lib/dns_server.ex
@@ -7,10 +7,18 @@ defmodule Gaia20.DNSServer do
   require Logger
   use DNS.Server
 
+  def generate_soa(record) do
+    {'gaia20.com', 'community.gaia20.com', 2018110201, 3600, 1800, 604800, 86400}
+  end
+
+  def generate_ns(record) do
+    'dns1.gaia20.org'
+  end
+
   @spec handle(%{anlist: any, header: %{qr: any}, qdlist: nonempty_maybe_improper_list}, any) ::
           %{anlist: [map], header: %{qr: false}, qdlist: []}
   def handle(record, _cl) do
-    my_public_ip = {68, 183, 208, 108}
+    my_public_ip = {35, 202, 252, 226}
 
     Logger.info(fn -> "#{inspect(record)}" end)
     query = hd(record.qdlist)
@@ -22,6 +30,8 @@ defmodule Gaia20.DNSServer do
         answer_data = case query.type do
           :a -> my_public_ip
           :txt -> ["gaia20-v0.0.1"]
+          :soa -> generate_soa(record)
+          :ns -> generate_ns(record)
           _ -> nil
         end
 
@@ -29,7 +39,7 @@ defmodule Gaia20.DNSServer do
           domain: query.domain,
           class: query.class,
           type: query.type,
-          ttl: 0,
+          ttl: 60,
           data: answer_data
         }
 
